f1da3e0c820de8e78269a4d0e6cca050e26bd3a2
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-07-23 14:36:38 +0100

    set up restricted middleware

--- users/authRoutes.js
+++ users/authRoutes.js
@@ -6,7 +6,9 @@ module.exports = router;
 
 router.post('/register', async (req, res) => {
   try {
-    const user = await createUser(req.body);
+    const { username, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const user = await createUser({username, password:hashedPassword});
     res.status(200).json(user);
   } catch (error) {
     console.log(error.message);
@@ -21,9 +23,12 @@ router.post('/login', async (req, res) => {
     if (user && bcrypt.compareSync(password, user.password)) {
       // eslint-disable-next-line require-atomic-updates
       req.session.user = user;
-      res.status(200).json({ message: `Welcome ${user.username}` });
+      res.status(200).json({
+        message: `Welcome ${user.username}`,
+        cookie: req.session.user.id
+      });
     } else {
-      res.status(401).json({ message: 'Invalid Credentials' });
+      res.status(401).json({ message: 'You shall not pass' });
     }
   } catch (error) {
     res.status(500).json({ error: error.message });
--- users/usersmodel.js
+++ users/usersmodel.js
@@ -1,20 +1,23 @@
 const db = require('../data/dbConfig');
-const bcrypt = require('bcryptjs');
 
 module.exports = {
   createUser,
-  findUsername
+  findUsername,
+  getUsers
 };
 
 function createUser(user) {
-  user.password = bcrypt.hashSync(user.password, 10);
   return db('users')
     .insert(user)
     .then(ids => findUser(ids[0]));
 }
 
+function getUsers() {
+  return db('users').select('username');
+}
+
 function findUser(id) {
-  return db('users').where({ id });
+  return db('users').select('id','username').where({ id }).first();
 }
 
 function findUsername(username) {
